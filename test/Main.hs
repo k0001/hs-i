5417d71da67712a0595e0dbbaec9b82a01f0a355
Renzo Carbonara <gnuk0001@gmail.com>
2023-03-20 21:14:29 +0200

    tests

--- test/Main.hs
+++ test/Main.hs
@@ -47,6 +47,7 @@ tt :: TestTree
 tt = testGroup "I"
   [ tt_Word8
   , tt_Int8
+  , tt_Natural
   ]
 
 --------------------------------------------------------------------------------
@@ -60,20 +61,6 @@ tt_Word8 = testGroup "Word8"
   [ testProperty "wrap" $ property $ do
       x <- forAll genWord8
       x === I.unwrap (I.wrap x)
-
-  , testCase "zero" $ do
-      0 @=? I.unwrap (I.zero @Word8 @0 @0)
-      0 @=? I.unwrap (I.zero @Word8 @0 @100)
-      0 @=? I.unwrap (I.zero @Word8 @0 @255)
-
-  , testCase "one" $ do
-      1 @=? I.unwrap (I.one @Word8 @0 @1)
-      1 @=? I.unwrap (I.one @Word8 @0 @100)
-      1 @=? I.unwrap (I.one @Word8 @0 @255)
-
-      1 @=? I.unwrap (I.one @Word8 @1 @1)
-      1 @=? I.unwrap (I.one @Word8 @1 @100)
-      1 @=? I.unwrap (I.one @Word8 @1 @255)
 
   , tt_Word8' @0   @0
   , tt_Word8' @0   @1
@@ -382,6 +369,152 @@ tt_Int8' = testGroup ("Interval [" <> show l <> ", " <> show r <> "]")
 
 --------------------------------------------------------------------------------
 
+tt_Natural :: TestTree
+tt_Natural = testGroup "Natural"
+  [ testProperty "wrap" $ property $ do
+      x <- forAll genNatural
+      x === I.unwrap (I.wrap x)
+
+  , tt_Natural'lr @0   @0
+  , tt_Natural'lr @0   @1
+  , tt_Natural'lr @0   @100
+  --, tt_Natural'l  @0   @'Nothing
+
+  , tt_Natural'lr @1   @1
+  , tt_Natural'lr @1   @100
+  --, tt_Natural'l  @1   @'Nothing
+
+  , tt_Natural'lr @10  @10
+  , tt_Natural'lr @10  @100
+  --, tt_Natural'l  @10  @'Nothing
+  ]
+
+tt_Natural'lr
+  :: forall (l :: I.L Natural) (r :: Natural)
+  .  I.Inhabited Natural l ('Just r)
+  => TestTree
+tt_Natural'lr = testGroup ("Interval [" <> show l <> ", " <> show r <> "]")
+  $ concat
+  [ pure $ testProperty "from" $ property $ do
+      x <- forAll genNatural
+      case I.from @Natural @l @('Just r) x of
+        Nothing -> assert (x < l' || x > r')
+        Just y -> do assert (x >= l' && x <= r')
+                     I.unwrap y === x
+
+  , pure $ testProperty "shove" $ property $ do
+      x <- forAll genNatural
+      let y = I.shove @Natural @l @('Just r) x
+      I.from (I.unwrap y) === Just y
+      if x < l' || x > r'
+         then I.from @Natural @l @('Just r) x === Nothing
+         else I.from @Natural @l @('Just r) x /== Nothing
+
+  , pure $ testProperty "plus'" $ property $ do
+      a <- forAll $ genINatural @l @('Just r)
+      b <- forAll $ genINatural @l @('Just r)
+      let x = toInteger (I.unwrap a) + toInteger (I.unwrap b)
+      case I.plus' a b of
+        Nothing -> assert (x < l'' || x > r'')
+        Just y -> toInteger (I.unwrap y) === x
+
+  , pure $ testProperty "mult'" $ property $ do
+      a <- forAll $ genINatural @l @('Just r)
+      b <- forAll $ genINatural @l @('Just r)
+      let x = toInteger (I.unwrap a) * toInteger (I.unwrap b)
+      case I.mult' a b of
+        Nothing -> assert (x < l'' || x > r'')
+        Just y -> toInteger (I.unwrap y) === x
+
+
+  , pure $ testProperty "minus'" $ property $ do
+      a <- forAll $ genINatural @l @('Just r)
+      b <- forAll $ genINatural @l @('Just r)
+      let x = toInteger (I.unwrap a) - toInteger (I.unwrap b)
+      case I.minus' a b of
+        Nothing -> assert (x < l'' || x > r'')
+        Just y -> toInteger (I.unwrap y) === x
+
+  , if (l' == 0 && r' == 0) then mzero else
+    pure $ testProperty "div'" $ property $ do
+      a <- forAll $ genINatural @l @('Just r)
+      b <- forAll $ Gen.filter (\x -> I.unwrap x /= 0)
+                               (genINatural @l @('Just r))
+      let (q, m) = toInteger (I.unwrap a) `divMod` toInteger (I.unwrap b)
+      case I.div' a b of
+        Nothing -> assert (q < l'' || q > r'' || m /= 0)
+        Just y -> do q === toInteger (I.unwrap y)
+                     m === 0
+
+  , pure $ testProperty "clamp'" $ property $ do
+      x <- forAll $ genNatural
+      case I.clamp @Natural @l @('Just r) x of
+        y | x < l' -> I.unwrap y === l'
+          | x > r' -> I.unwrap y === r'
+          | otherwise -> Just y === I.from x
+
+  , pure $ testProperty "with" $ property $ do
+      x <- forAll $ genINatural @l @('Just r)
+      x === I.with x I.known'
+
+
+  , case L.cmpNat (Proxy @l) (Proxy @r) of
+      LTI ->
+        [ testProperty "pred" $ property $ do
+            x <- forAll $ genINatural @l @('Just r)
+            case I.pred' x of
+              Nothing -> x === l
+              Just y -> do x /== l
+                           I.unwrap y === I.unwrap x - 1
+        , testProperty "succ" $ property $ do
+            x <- forAll $ genINatural @l @('Just r)
+            case I.succ' x of
+              Nothing -> x === r
+              Just y -> do x /== r
+                           I.unwrap y === I.unwrap x + 1
+        ]
+      _ -> mzero
+
+  , case L.cmpNat (Proxy @l) (Proxy @0) of
+      EQI -> pure $ testCase "zero" $
+               0 @=? I.unwrap (I.zero @Natural @l @('Just r))
+      _ -> mzero
+
+
+  , case (leNatural @l @1, leNatural @1 @r) of
+      (Just Dict, Just Dict) -> pure $ testCase "one" $ do
+        1 @=? I.unwrap (I.one @Natural @l @('Just r))
+      _ -> mzero
+
+  , pure $ testProperty "negate'" $ property $ do
+      x <- forAll $ genINatural @l @('Just r)
+      Nothing === I.negate' x
+
+  , pure $ testProperty "down" $ property $ do
+      x <- forAll $ genINatural @l @('Just r)
+      Just x === I.down x
+      case I.down x of
+        Nothing -> failure
+        Just y -> I.unwrap x
+              === I.unwrap (y :: I Natural (I.MinL Natural) (I.MaxR Natural))
+
+  , pure $ testProperty "up" $ property $ do
+      x <- forAll $ genINatural @l @('Just r)
+      x === I.up x
+      I.unwrap x ===
+        I.unwrap (I.up x :: I Natural (I.MinL Natural) (I.MaxR Natural))
+
+  ]
+  where
+    l   = I.min        :: I Natural l ('Just r)
+    l'  = I.unwrap l   :: Natural
+    l'' = toInteger l' :: Integer
+    r   = I.max        :: I Natural l ('Just r)
+    r'  = I.unwrap r   :: Natural
+    r'' = toInteger r' :: Integer
+
+--------------------------------------------------------------------------------
+
 genWord8 :: MonadGen m => m Word8
 genWord8 = Gen.integral $ Range.constant minBound maxBound
 
@@ -442,13 +575,9 @@ leInteger = case KI.cmpInteger (Proxy @a) (Proxy @b) of
   L.EQI -> Just $ unsafeCoerce (Dict @())
   L.GTI -> Nothing
 
-negateInteger
-  :: forall (a :: KI.Integer)
-  .  (KI.KnownInteger a) :- (KI.KnownInteger (KI.Negate a))
-negateInteger = integerMagic1 negate
 
 --------------------------------------------------------------------------------
--- TODO: Move this stuff from Data.Constraint.Nat to KindInteger.
+-- TODO: Move this stuff to KindInteger (ideas from Data.Constraint.Nat)
 
 newtype IntegerMagic n =
   IntegerMagic (KI.KnownInteger n => Dict (KI.KnownInteger n))
@@ -461,4 +590,9 @@ integerMagic1
   -> (KI.KnownInteger a :- KI.KnownInteger b)
 integerMagic1 f =
   Sub $ unsafeCoerce (IntegerMagic Dict) $ f (KI.integerVal (Proxy @a))
+
+negateInteger
+  :: forall (a :: KI.Integer)
+  .  (KI.KnownInteger a) :- (KI.KnownInteger (KI.Negate a))
+negateInteger = integerMagic1 negate
 
