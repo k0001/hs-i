8c83f89db04b803b9a39eaa78750ff284003cd1d
Renzo Carbonara <gnuk0001@gmail.com>
2023-03-25 09:59:44 +0200

    flake

--- flake.nix
+++ flake.nix
@@ -2,70 +2,48 @@
   description = "Haskell 'i' library";
 
   inputs = {
-    hs_kind.url = "github:k0001/hs-kind?ref=kind-rational-0.2";
+    nixpkgs.url =
+      "github:NixOS/nixpkgs?rev=21eda9bc80bef824a037582b1e5a43ba74e92daa";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    hs_kind.url = "github:k0001/hs-kind";
     hs_kind.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, hs_kind }:
-    let
-      inherit (nixpkgs) lib;
-      hs_i = import ./.;
-      hspkgsOverrides = pself: psuper: hself: hsuper: {
-        kind-integer = hsuper.callPackage hs_kind.hs_kind-integer { };
-        kind-rational = hsuper.callPackage hs_kind.hs_kind-rational { };
-        i = hsuper.callPackage hs_i { };
-      };
-      pkgsOverlay = pself: psuper: {
-        haskell = psuper.haskell // {
-          packageOverrides = hspkgsOverrides pself psuper;
-        };
-      };
-      pkgsFor = system:
-        import nixpkgs {
+  outputs = inputs@{ ... }:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.flake-parts.flakeModules.easyOverlay ];
+      systems = [ "x86_64-linux" "i686-linux" "aarch64-linux" ];
+      perSystem = { config, pkgs, final, system, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
           inherit system;
-          overlays = [ pkgsOverlay ];
+          overlays = [ inputs.hs_kind.overlays.default ];
         };
-
-    in {
-      inherit hs_i hspkgsOverrides pkgsOverlay;
-      packages = lib.genAttrs [ "x86_64-linux" "i686-linux" "aarch64-linux" ]
-        (system:
-          let pkgs = pkgsFor system;
-          in {
-            default = pkgs.releaseTools.aggregate {
-              name = "every output from this flake";
-              constituents = let
-                p = self.packages.${system};
-                s = self.devShells.${system};
-              in [
-                # p.hs_i__ghcDefault
-                p.hs_i__ghc943
-
-                # p.hs_i__ghcDefault.doc
-                p.hs_i__ghc943.doc
-
-                # s.hs_i__ghcDefault
-                s.hs_i__ghc943
-              ];
-            };
-            # hs_i__ghcDefault = pkgs.haskellPackages.i;
-            hs_i__ghc943 = pkgs.haskell.packages.ghc943.i;
-          });
-      devShells = lib.genAttrs [ "x86_64-linux" "i686-linux" "aarch64-linux" ]
-        (system:
-          let
-            pkgs = pkgsFor system;
-            mkShellFor = hpkgs:
-              hpkgs.shellFor {
-                packages = p: [ p.i ];
-                withHoogle = true;
-                nativeBuildInputs = [ pkgs.cabal-install pkgs.cabal2nix ];
-              };
-          in {
-            default = self.devShells.${system}.hs_i__ghc943;
-            # hs_i__ghcDefault = mkShellFor pkgs.haskellPackages;
-            hs_i__ghc943 = mkShellFor pkgs.haskell.packages.ghc943;
-          });
+        overlayAttrs = {
+          haskell = pkgs.haskell // {
+            packageOverrides = pkgs.lib.composeExtensions
+              (pkgs.haskell.packageOverrides or (_: _: { }))
+              (hself: hsuper: { i = hself.callPackage ./. { }; });
+          };
+        };
+        packages = {
+          i__ghc943 = final.pkgs.haskell.packages.ghc943.i;
+          default = pkgs.releaseTools.aggregate {
+            name = "every output from this flake";
+            constituents = [
+              config.packages.i__ghc943
+              config.packages.i__ghc943.doc
+              config.devShells.ghc943
+            ];
+          };
+        };
+        devShells = {
+          default = config.devShells.ghc943;
+          ghc943 = final.pkgs.haskell.packages.ghc943.shellFor {
+            packages = p: [ p.i ];
+            withHoogle = true;
+            nativeBuildInputs = [ pkgs.cabal-install pkgs.cabal2nix ];
+          };
+        };
+      };
     };
-
 }
